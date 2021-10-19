eeea9c33ec061af0506b05954d48ba5286a0d24b
Sharonoito <oitosharonakoth@gmail.com>
2021-10-19 15:33:34 +0300

    login

--- RedSwift/settings.py
+++ RedSwift/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 import django_heroku
 
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'login',
             ],
         },
     },
@@ -84,6 +86,11 @@ DATABASES = {
         'PORT': '',
     }
 }
+
+
+
+
+
 
 
 # Password validation
@@ -122,7 +129,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+
 STATIC_URL = '/static/'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+MEDIA_URL='/media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
