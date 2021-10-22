6b64ee912985621323cabde5b7f494be14e83133
Sharonoito <oitosharonakoth@gmail.com>
2021-10-22 08:25:19 +0300

    authenticate

--- RedSwift/urls.py
+++ RedSwift/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # path('',include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include("registration.backends.default.urls")),
+    path('authentication/',include("authentication.urls")),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-
-]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 def login (request):
@@ -8,3 +10,13 @@ def login (request):
     else:
         return redirect("auth_login")
        
+
+ def email(request):
+    subject = 'Thank you for registering to our site'
+    message = ' it  means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['receiver@gmail.com',]
+    send_mail( subject, message, email_from, recipient_list )
+    return redirect('redirect to a new page')      
+
+send_mail('Subject here', 'Here is the message.', 'from@example.com', ['to@example.com'], fail_silently=False)
